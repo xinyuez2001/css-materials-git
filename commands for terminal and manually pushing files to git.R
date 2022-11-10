d1bdf48bac0ddae4d64361059a5377fd479dfb03
xinyuez2001 <xinyuez2001@uchicago.edu>
2022-11-10 16:49:42 -0600

    change YAML header

---
+++ commands for terminal and manually pushing files to git.R
@@ -0,0 +1,16 @@
+terminal/command line commands:
+pwd give the current directory 
+cd navigate around directory for example cd css-materials-git
+ls look in what is inside the current directory
+ls -a see hidden content in the repository like Rhistory
+to get out of problem in terminal :q
+touch for example git-script.R add new file to the current directory in terminal
+
+git command:
+  
+git init initialize empty git repository
+git status check git status
+git add tell git to track file
+git add . tell git to track all files, equal to stage
+git commit -m "message with commit" equal to commit
+git push equal to push, the file would be pushed to the repository
